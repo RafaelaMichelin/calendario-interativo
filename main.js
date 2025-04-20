ca1de71a24efda061e957b77fca82643d933cc7f
RafaelaMichelin <Rafaelamichelin@gmail.com>
2025-04-20 15:45:50 -0300

    Criando função colorirDia e armazenando o valor de campo dia, color e calendar nas variáveis criada

---
+++ main.js
@@ -0,0 +1,7 @@
+//Criando função
+function colorirDia(){
+    let days = document.getElementById('day').value;
+    let color = document.getElementById('color').value;
+    let calendar = document.getElementById('calendar');
+    
+}
