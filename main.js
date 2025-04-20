4007f5817b494db321fe10119b230770968c1ba7
RafaelaMichelin <Rafaelamichelin@gmail.com>
2025-04-20 16:04:12 -0300

    Criando condição para conferir se o campo dia foi inserido, se tiver inserido implementando uma variável que passe por todos td que tiver no calendario, assim pegando o numero do campo dia inserido e mudando a cor referente a cor armazenada

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 //Criando função
 function colorirDia(){
+    //armazenando as infomações dos campos nas variáveis
     let days = document.getElementById('day').value;
     let color = document.getElementById('color').value;
     let calendar = document.getElementById('calendar');
-    
+     
+    //Conferindo se o dia foi inserido
+    if(!days){
+        alert('Favor informar o campo dia');
+    }
+    else if((days > 0)&&(days < 31)){
+        //Se o numero de dia está no intervalo será aplicado a cor no calendário
+        let td = calendar.getElementsByTagName('td')[parseInt(days)+1];
+        td.style.backgroundColor = color;
+    }
+    else{
+        alert('Favor informar um dia do calendario');
+    }
 }
