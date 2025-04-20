b4b6fb280c1f91243553c43fc1cda8af5cce638e
RafaelaMichelin <Rafaelamichelin@gmail.com>
2025-04-20 16:23:01 -0300

    Criando contador de cores, com um loop FOR para verificar o estilo do elemento e condições if else para verificar a cor selecionada e contar quantas vezes foi adicionada

--- main.js
+++ main.js
@@ -17,4 +17,34 @@ function colorirDia(){
     else{
         alert('Favor informar um dia do calendario');
     }
+    //Criando contador
+    let elementos = document.querySelectorAll('td');
+    let contadorAzul = 0, contadorVerde = 0, contadorRosa = 0, contadorVermelho = 0;
+    
+    for(let i=0; i<elementos.length;i++){
+        let estilo = window.getComputedStyle(elementos[i]);
+        let corEstilo = estilo.backgroundColor;
+        
+        //condição
+        if(corEstilo === "LightBlue"|| corEstilo === "rgb(173, 216, 230)")
+        {
+            contadorAzul++;
+            console.log("Azul:"+contadorAzul);
+        }
+        else if (corEstilo === "PaleGreen" || corEstilo === "rgb(152, 251, 152)")
+        {
+            contadorVerde++;
+            console.log("Verde:"+contadorVerde);
+        }
+        else if (corEstilo === "Pink"|| corEstilo === "rgb(255, 192, 203)")
+        {
+            contadorRosa++;
+            console.log("Rosa:"+contadorRosa);
+        }
+        else if(corEstilo === "Red" || corEstilo === "rgb(255, 0, 0)")
+        {
+            contadorVermelho++;
+            console.log("Vermelho:"+contadorVermelho);
+        }
+    }
 }
